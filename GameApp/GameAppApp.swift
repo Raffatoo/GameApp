20f41b3e6f12e1e3b71684d3d4ce58346b4b556d
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2021-10-12 04:02:01 -0500

    Game App
    inicio GameApp

---
+++ GameApp/GameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppApp.swift
+//  GameApp
+//
+//  Created by Cruz Torres on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
